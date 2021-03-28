c51609aea16853f0066526b7bac8ebafd9df5e31
Flavio fernando da Silva <tchfernando@gmail.com>
2021-03-28 15:47:01 -0300

    loop while

---
+++ br/com/treinamento/MediaAlunos.java
@@ -0,0 +1,27 @@
+package br.com.treinamento;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MediaAlunos {
+  public static void main(String[] args) {
+    float notaAluno = 0;
+    float totalNota = 0;
+    float mediaNota = 0;
+    int totalAluno = 0;
+
+    Scanner entrada = new Scanner(System.in);
+    entrada.useLocale(Locale.US);
+
+    for (int i = 0; i < 3; i++) {
+      System.out.print("Insira uma nota para continuar -> ");
+      notaAluno = entrada.nextFloat();
+      totalNota += notaAluno;
+      totalAluno++;
+    }
+    entrada.close();
+
+    mediaNota = totalNota / totalAluno;
+    System.out.print("A media da sala, para " + totalAluno + " alunos, foi de " + mediaNota);
+  }
+}
