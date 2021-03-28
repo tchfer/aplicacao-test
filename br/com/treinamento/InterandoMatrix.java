e680d6534a0523bf3fa3514d78ecdf91fba6ed15
Flavio fernando da Silva <tchfernando@gmail.com>
2021-03-28 16:00:41 -0300

    Enhanced for

---
+++ br/com/treinamento/InterandoMatrix.java
@@ -0,0 +1,10 @@
+package br.com.treinamento;
+
+public class InterandoMatrix {
+  public static void main(String[] args) {
+    int arr[] = {1, 2, 3, 4, 5};
+    for(int valor: arr) {
+      System.out.println(valor);
+    }
+  }
+}
